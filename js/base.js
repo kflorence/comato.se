3a3467ae18f84f810964331930e47006f44a76ae
Kyle Florence <kyle.florence@gmail.com>
2011-06-12 20:16:41 -0700

    Further refine the layout

--- js/base.js
+++ js/base.js
@@ -1,9 +1,1 @@
-jQuery(function($) {
-	var $toggle = $("#toggleComments a"),
-		$comments = $("#comments");
-
-	$toggle.click(function() {
-		$toggle.text( ( $comments.is(":hidden") ? "Hide" : "Show" ) + " Comments");
-		$comments.slideToggle("slow");
-	});
-});
+jQuery(function($) {});
